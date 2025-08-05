15e1d1a55dab7cf2cf22d0a2a23ad9bc804dad10
fairofif <fairofif@gmail.com>
2025-08-05 15:37:41 +0700

    push auth-lib init

---
+++ src/test/java/com/trymicroservices/auth_lib/AuthLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trymicroservices.auth_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
